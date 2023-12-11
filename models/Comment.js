e95330b4fcf80cc3c39e1b07a531d84bafa36da6
Anoop Singh <anoop102910@gmail.com>
2023-12-11 15:52:25 +0530

    added comment functionality

---
+++ models/Comment.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User', 
+    required: true,
+  },
+  postId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Post', 
+    required: true,
+  },
+ 
+},
+{
+    timestamps:true
+});
+
+const Comment = mongoose.model('Comment', commentSchema);
+
+module.exports = Comment;
